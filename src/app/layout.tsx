28eee5d2911c897371db950ecd8d2809abba8a74
junjuny0227 <junjuny0227@gmail.com>
2025-07-01 22:22:18 +0900

    feat: 메타데이터 title 및 description 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "",
-  description: "",
+  title: "Ready, GSM",
+  description:
+    "Ready, GSM은 광주소프트웨어마이스터고 학과체험 및 입학설명회 신청 서비스입니다",
 };
 
 const RootLayout = ({
