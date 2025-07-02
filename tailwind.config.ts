66452f85f2724e4ab2d66a11f1c0cd18b5ea0f0c
junjuny0227 <junjuny0227@gmail.com>
2025-07-02 16:11:09 +0900

    feat: tailwind 커스텀 컬러 팔레트 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,18 @@ const config: Config = {
     './src/pageContainer/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#3B82F6',
+        sub: '#EFF6FF',
+        background: '#FAFAFA',
+        gray: '#A8A8A8',
+        black: '#121212',
+        red: '#EF4444',
+        green: '#10B981',
+        purple: '#6366F1',
+      },
+    },
   },
   plugins: [],
 };
