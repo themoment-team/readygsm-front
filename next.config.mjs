21673f51620d4ed12080186af156e8bc0bef69fa
junjuny0227 <junjuny0227@gmail.com>
2025-07-08 18:08:20 +0900

    style: 코드 포맷팅 적용

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
+        source: '/api/:path*',
         destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
       },
     ];
