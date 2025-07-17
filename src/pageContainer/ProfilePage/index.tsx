582a39bde772f4fa51c808aa7719fcbc08b70fba
yeondon <s25030@gsm.hs.kr>
2025-07-17 23:22:38 +0900

    기본 퍼블리싱

--- src/pageContainer/ProfilePage/index.tsx
+++ src/pageContainer/ProfilePage/index.tsx
@@ -1,70 +1,66 @@
 import { cn } from '@/lib/utils';
 
-const Hello: React.FC<{ username: string }> = ({ username }) => {
+const Hello: React.FC<{ email: string }> = ({ email }) => {
   return (
-    <div
-      className={cn(
-        'text-black',
-        'text-center',
-        'text-[2.5rem]',
-        'not-italic',
-        'font-normal',
-        'leading-normal',
-        'h-[5.125rem]',
-        'relative',
-        'flex',
-        'flex-col',
-        'items-center',
-        'justify-center',
-      )}
-    >
-      <div>{username}님, 반갑습니다!</div>
+    <div className={cn('flex', 'flex-col', 'justify-center', 'items-center', 'gap-[1.25rem]')}>
       <div
         className={cn(
-          'rounded-[9.375rem]',
-          'bg-[rgba(156,194,255,0.15)]',
-          'blur-[30px]',
-          'w-[94.05%]',
-          'h-[5.125rem]',
-          'absolute',
-          'top-1/2',
-          'left-1/2',
-          'transform',
-          '-translate-x-1/2',
-          '-translate-y-1/2',
+          'rounded-[1.5rem]',
+          'bg-[var(--gray,#E2E8F0)]',
+          'flex',
+          'w-[5.5rem]',
+          'h-[5.5rem]',
+          'px-[1.5rem]',
+          'py-[1.375rem]',
+          'items-center',
         )}
       ></div>
+      <div
+        className={cn(
+          'font-[Pretendard]',
+          'text-[1.5rem]',
+          'tracking-[-0.009rem]',
+          'text-[var(--blue2,#0C4A6E)]',
+        )}
+      >
+        {email}
+      </div>
     </div>
   );
 };
 
-const Email: React.FC<{ email: string }> = ({ email }) => {
-  return (
-    <div
-      className={cn(
-        'flex',
-        'flex-col',
-        'items-center',
-        'justify-center',
-        'text-[1.5rem]',
-        'gap-[1.5rem]',
-      )}
-    >
-      <div>프로필</div>
-      <div>{email}</div>
-    </div>
-  );
+const Box = ({}) => {
+  return <div></div>;
 };
 
 const ProfilePage = () => {
-  const username = 'example123';
   const email = 'example123@gmail.com';
   return (
     <div className={cn('bg-background', 'h-screen', 'font-[Pretendard]')}>
-      <div className={cn('mt-16', 'flex', 'flex-col', 'items-center', 'justify-center', 'gap-4')}>
-        <Hello username={username} />
-        <div className={cn('flex', 'flex-col', 'items-center', 'justify-center', 'gap-[7rem]')}>
-          <Email email={email} />
+      <div //이미지 + 유저 아이디
+        className={cn(
+          'mt-[10.38rem]',
+          'flex',
+          'flex-col',
+          'items-center',
+          'justify-center',
+          'gap-4',
+        )}
+      >
+        <Hello email={email} />
+        <div className={cn('mt-[6.75rem]', 'flex', 'flex-col', 'gap-[1.81rem]')}>
+          <div
+            className={cn(
+              'w-[88rem]',
+              'text-[var(--Header-text,#111827)]',
+              'font-[Pretendard]',
+              'text-[1.75rem]',
+              'leading-normal',
+            )}
+          >
+            신청된 프로그램
+          </div>
+          <Box></Box>
         </div>
       </div>
     </div>
