53d4dd5d23c844eec4b3d6d4e0ebc24e5ee1d896
yeondon <s25030@gsm.hs.kr>
2025-07-23 19:29:35 +0900

    기본 퍼블리싱

--- src/pageContainer/Adminmain/index.tsx
+++ src/pageContainer/Adminmain/index.tsx
@@ -1,5 +1,12 @@
+import { cn } from '@/lib/utils';
+
 const Adminmain = () => {
-  return <div>Adminmain</div>;
+  return (
+    <div className={cn('flex h-screen w-full flex-col items-center justify-center')}>
+      <div>2025 READY,GSM ADMIN SERVICE</div>
+      <div>광주소프트웨어 마이스터고등학교 신청 서비스</div>
+    </div>
+  );
 };
 
 export default Adminmain;
