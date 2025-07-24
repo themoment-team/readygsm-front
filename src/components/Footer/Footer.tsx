6352d8164b9a57582fd16348cb48505929aaf79e
서연 <s25037@gsm.hs.kr>
2025-07-24 16:40:07 +0900

    [fix] 리뷰 요청사항 반영 및 수정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,27 +1,27 @@
-'use client';
-
 import Logo from '@/asset/logo';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
-    <footer className="bg-[#F9FAFB] px-6 py-10 text-[13px] text-[#9CA3AF] sm:px-10">
-      <div className="mx-auto flex max-w-7xl flex-col justify-between gap-8 sm:flex-row">
-        <div className="hidden items-center sm:flex sm:flex-col sm:items-start">
+    <footer className="bg-gray-50 px-6 py-10 text-[13px] text-gray-400 tablet-sm:px-10">
+      <div className="mx-auto flex max-w-7xl flex-col justify-between gap-8 tablet-sm:flex-row">
+        <div className="hidden items-center tablet-sm:flex tablet-sm:flex-col tablet-sm:items-start">
           <Logo />
         </div>
 
-        <div className="flex flex-col items-center text-[13px] text-[#4B5563] sm:items-end">
-          <div className="text-[1.125rem] font-normal text-[#6B7280]">
-            © 2025 Ready,GSM. All rights reserved.
+        <div className="flex flex-col items-center text-[13px] text-gray-600 tablet-sm:items-end">
+          <div className="text-[1.125rem] font-normal text-gray-500">
+            © {year} Ready,GSM. All rights reserved.
           </div>
 
-          <div className="mt-4 flex gap-6 text-[1.25rem] font-bold text-[#4B5563]">
+          <div className="mt-[1rem] flex gap-6 text-[1.25rem] font-bold text-gray-600">
             <a href="http://gsm.gen.hs.kr/main/main.php">홈페이지</a>
             <a href="https://official.hellogsm.kr/about/location">찾아오시는 길</a>
             <a href="http://gsm.gen.hs.kr/sub/page.php?page_code=info_14">교내 배치도</a>
           </div>
 
-          <div className="mt-16 text-center text-[1rem] leading-relaxed text-[#9CA3AF] sm:text-right">
+          <div className="mt-[4rem] text-center text-[1rem] leading-relaxed text-gray-400 tablet-sm:text-right">
             우) 62423 광주광역시 광산구 상무대로 312
             <br />
             담당자) 062-949-6843 (8:30~16:30)
