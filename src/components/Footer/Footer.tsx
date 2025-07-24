deefb64ca481203debea44b6b016dcd84cd364bc
서연 <s25037@gsm.hs.kr>
2025-07-24 11:41:32 +0900

    반응형 적용

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,17 +4,18 @@ import Logo from '@/asset/logo';
 
 const Footer = () => {
   return (
-    <footer className="bg-[#EEEFEF] px-6 py-10 sm:px-10">
-      <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-8 sm:flex-row sm:items-start">
-        <div className="mt-6 flex flex-col items-center justify-center sm:items-start">
+    <footer className="bg-[#F9FAFB] px-6 py-10 text-[13px] text-[#9CA3AF] sm:px-10">
+      <div className="mx-auto flex max-w-7xl flex-col justify-between gap-8 sm:flex-row">
+        <div className="hidden items-center sm:flex sm:flex-col sm:items-start">
           <Logo />
         </div>
 
-        <div className="text-gray-600 space-y-2 text-right text-sm sm:text-right">
-          <div className="text-base font-semibold text-[#121212]">
-            광주소프트웨어마이스터고등학교
+        <div className="flex flex-col items-center text-[13px] text-[#4B5563] sm:items-end">
+          <div className="text-[1.125rem] font-normal text-[#6B7280]">
+            © 2025 Ready,GSM. All rights reserved.
           </div>
-          <div className="flex justify-center gap-6 text-[13px] sm:justify-end">
+
+          <div className="mt-4 flex gap-6 text-[1.25rem] font-bold text-[#4B5563]">
             <a href="#" className="hover:underline">
               홈페이지
             </a>
@@ -26,17 +27,12 @@ const Footer = () => {
             </a>
           </div>
 
-          <div className="h-6" />
-
-          <div className="text-[12px] leading-5 text-[#666666]">
+          <div className="mt-16 text-center text-[1rem] leading-relaxed text-[#9CA3AF] sm:text-right">
             우) 62423 광주광역시 광산구 상무대로 312
             <br />
             담당자) 062-949-6843 (8:30~16:30)
             <br />
             교무실) 062-949-6800 (08:30~16:30)
-          </div>
-          <div className="mt-8 text-[12px] text-[#666666]">
-            © 2025 Ready,GSM. All rights reserved.
           </div>
         </div>
       </div>
