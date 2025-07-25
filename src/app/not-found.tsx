60e745b59f7a1aefc78d9e70115bc6d6815dc1bf
서연 <s25037@gsm.hs.kr>
2025-07-25 23:56:26 +0900

    404 Router 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,10 +5,17 @@ import { useRouter } from 'next/navigation';
 import Four from '@/asset/four';
 import Four2 from '@/asset/four2';
 import Zero from '@/asset/zero';
-import { cn } from '@/lib/utils';
 
 const NotFound = () => {
   const router = useRouter();
+
+  const GoBack = () => {
+    if (document.referrer) {
+      router.back();
+    } else {
+      router.push('/');
+    }
+  };
 
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center px-4 text-center">
@@ -26,15 +33,16 @@ const NotFound = () => {
         </div>
       </div>
 
-      <p className="mt-20 text-[24px] font-bold text-black mobile:text-[18px] mobile-lg:text-[20px] tablet-sm:text-[22px] tablet:text-[24px]">
+      <p className="mt-20 text-[1.5rem] font-bold text-black mobile:text-[1.125rem] mobile-lg:text-[1.25rem] tablet-sm:text-[1.375rem] tablet:text-[1.5rem]">
         페이지를 찾을 수 없음
       </p>
-      <p className="mt-4 text-[24px] text-gray-400 mobile:text-[14px] mobile-lg:text-[16px] tablet-sm:text-[18px] tablet:text-[20px]">
+
+      <p className="mt-4 text-[1.5rem] text-gray-400 mobile:text-[0.875rem] mobile-lg:text-[1rem] tablet-sm:text-[1.125rem] tablet:text-[1.25rem] tablet-lg:text-[1.375rem] desktop-sm:text-[1.5rem] desktop:text-[1.625rem] desktop-lg:text-[1.75rem] desktop-xl:text-[1.875rem]">
         입력하신 페이지의 주소가 정확한지 다시 한번 확인해 주세요.
       </p>
 
       <button
-        onClick={() => router.push('/')}
+        onClick={() => GoBack()}
         className="mt-8 flex items-center space-x-2 rounded-full border border-gray-400 bg-white px-6 py-2 text-gray-500 transition hover:bg-gray-100"
       >
         <svg
