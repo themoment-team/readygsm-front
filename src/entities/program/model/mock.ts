647c85664316b7dc33d66b5bbb1486a2483393a8
yeondon <s25030@gsm.hs.kr>
2026-04-01 20:40:57 +0900

    feat : mock data 추가

---
+++ src/entities/program/model/mock.ts
@@ -0,0 +1,34 @@
+import type { programComponentProps } from './types';
+
+export const projectMockList: programComponentProps[] = [
+  {
+    title: '프로젝트 A',
+    content: [
+      '간단한 웹 페이지(HTML/CSS) 제작 체험',
+      '기초 프로그래밍(Python)을 활용한 문제 해결 실습',
+      '팀 프로젝트를 통한 미니 서비스 기획 및 개발 경험',
+    ],
+    date: '2023-10-01 (목)',
+    personnel: 5,
+  },
+  {
+    title: '프로젝트 B',
+    content: [
+      '간단한 웹 페이지(HTML/CSS) 제작 체험',
+      '기초 프로그래밍(Python)을 활용한 문제 해결 실습',
+      '팀 프로젝트를 통한 미니 서비스 기획 및 개발 경험',
+    ],
+    date: '2023-10-01 (목)',
+    personnel: 5,
+  },
+  {
+    title: '프로젝트 C',
+    content: [
+      '간단한 웹 페이지(HTML/CSS) 제작 체험',
+      '기초 프로그래밍(Python)을 활용한 문제 해결 실습',
+      '팀 프로젝트를 통한 미니 서비스 기획 및 개발 경험',
+    ],
+    date: '2023-10-01 (목)',
+    personnel: 5,
+  },
+];
---
+++ src/entities/program/model/types.ts
@@ -0,0 +1,6 @@
+export interface programComponentProps {
+  title: string;
+  content: string[];
+  date: string;
+  personnel: number;
+}
