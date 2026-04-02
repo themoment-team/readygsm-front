3c942cc2d6d1890bac5b1a7fae8171e854fee67d
yeondon <s25030@gsm.hs.kr>
2026-04-02 16:58:22 +0900

    feat : PretendardVariable로 폰트 변경

--- src/shared/styles/fonts.ts
+++ src/shared/styles/fonts.ts
@@ -1,22 +1,8 @@
 import localFont from 'next/font/local';
 
 export const pretendard = localFont({
-  src: [
-    {
-      path: '../../../public/fonts/Pretendard-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../../public/fonts/Pretendard-Medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../../../public/fonts/Pretendard-Bold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
+  src: '../public/fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+  weight: '100 900',
   display: 'swap',
 });
