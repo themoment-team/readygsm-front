1688de846ec902d6207c369f134f1a0eb4af846b
seoxeon09 <s25037@gsm.hs.kr>
2026-04-17 10:11:12 +0900

    refactor: 404 페이지에서 router.push를 Link로 교체 및 use client 제거

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,9 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 import { cn } from '@/shared/lib';
-import { Button } from '@/shared/ui';
+import { buttonVariants } from '@/shared/ui';
 
 const NotFound = () => {
-  const router = useRouter();
-
   return (
     <div className={cn('flex h-[calc(100vh-6.25rem)] flex-col items-center justify-center gap-4')}>
       <h1 className={cn('text-brand-primary text-center text-5xl leading-[3.6rem] font-bold')}>
@@ -17,9 +13,9 @@ const NotFound = () => {
         잘못된 경로입니다. 존재하지 않는 주소를 입력하셨거나, <br />
         요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.
       </p>
-      <Button variant="default" size="md" onClick={() => router.push('/')}>
+      <Link href="/" className={cn(buttonVariants({ variant: 'default', size: 'md' }))}>
         홈 화면으로 이동
-      </Button>
+      </Link>
     </div>
   );
 };
