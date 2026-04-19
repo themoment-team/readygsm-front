17b71f3858c938caf2b02b7ec9ec0a0260811c46
yeondon <s25030@gsm.hs.kr>
2026-04-19 14:06:48 +0900

    feat: navigation 분리

---
+++ src/widgets/footer/model/navigation.ts
@@ -0,0 +1,18 @@
+export const LINKS = [
+  {
+    text: '개인정보처리방침',
+    link: 'https://official.hellogsm.kr/policy/privacy',
+  },
+  {
+    text: '영상정보처리기기운영·관리방침',
+    link: 'https://official.hellogsm.kr/policy/cctv',
+  },
+  {
+    text: '저작권신고 및 보호규정',
+    link: 'https://official.hellogsm.kr/policy/copyright',
+  },
+  {
+    text: '찾아오시는길',
+    link: 'https://official.hellogsm.kr/about/location',
+  },
+] as const;
--- src/widgets/footer/ui/index.tsx
+++ src/widgets/footer/ui/index.tsx
@@ -1,25 +1,7 @@
 import { cn } from '@/shared/lib';
 
+import { LINKS } from '../model/navigation';
 import FooterGSMLogo from './FooterGSMLogo';
-
-const LINKS = [
-  {
-    text: '개인정보처리방침',
-    link: 'https://official.hellogsm.kr/policy/privacy',
-  },
-  {
-    text: '영상정보처리기기운영·관리방침',
-    link: 'https://official.hellogsm.kr/policy/cctv',
-  },
-  {
-    text: '저작권신고 및 보호규정',
-    link: 'https://official.hellogsm.kr/policy/copyright',
-  },
-  {
-    text: '찾아오시는길',
-    link: 'https://official.hellogsm.kr/about/location',
-  },
-] as const;
 
 const Footer = () => {
   const CURRENT_YEAR = new Date().getFullYear();
