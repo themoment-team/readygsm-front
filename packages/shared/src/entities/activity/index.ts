b87a9710981dba9bcfb25cf09ea1e19d669077d3
seoxeon09 <s25037@gsm.hs.kr>
2026-09-10 08:28:36 +0900

    refactor: 사용하지 않는 레거시 코드 제거
    - ProgramList.tsx: 어디서도 import되지 않는 미사용 컴포넌트 삭제
    - useGetActivityById: barrel export만 있고 실제 사용처가 없는 훅 삭제
    - useGetMyApplication: barrel export만 있고 실제 사용처가 없는 훅 삭제

--- packages/shared/src/entities/activity/index.ts
+++ packages/shared/src/entities/activity/index.ts
@@ -4,6 +4,5 @@ export { default as getActivityList } from './api/getActivityList';
 export { revalidateActivityList } from './api/revalidateActivityList';
 export { MAX_RESERVE_APPLICANT } from './model/constants';
 export type { ActivityListResponseType, ActivityType } from './model/types';
-export { default as useGetActivityById } from './model/useGetActivityById';
 export { activityQueryKeys, default as useGetActivityList } from './model/useGetActivityList';
 export { default as ActivityCard } from './ui/ActivityCard';
